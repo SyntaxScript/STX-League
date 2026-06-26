48fff8b86e607fc9823fef2e89926bb57d2c4086
Syntax. <thedrymendry@gmail.com>
2026-06-26 17:49:55 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@
 
         //  true — регистрация открыта
         //  false — регистрация закрыта
-        REGISTRATION_OPEN: true,
+        REGISTRATION_OPEN: false,
         REGISTRATION_CLOSED_MESSAGE: 'Регистрация на турнир пока закрыта',
         REGISTRATION_CLOSED_HINT: 'Скоро мы откроем приём заявок. Следите за новостями в Discord и Telegram!'
     };
